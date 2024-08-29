2c923b9cabb270dc514651200546a2ca1bdff096
Bishwas-paudel <paudelbishwas885@gmail.com>
2024-08-29 21:15:39 +0545

    Upadate and delete

--- StudentPortal/Controllers/StudentsController.cs
+++ StudentPortal/Controllers/StudentsController.cs
@@ -42,16 +42,51 @@ namespace StudentPortal.Controllers
             return View();
         }
         [HttpGet]
-        public async Task <IActionResult> List()
+        public async Task<IActionResult> List()
         {
-           var students =  await _dbContext.Students.ToListAsync();
+            var students = await _dbContext.Students.ToListAsync();
 
-
+            return View(students);
 
 
         }
-    
-    
-    
+
+        [HttpGet]
+        public async Task<IActionResult> edit(Guid id)
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> edit(Student viewModel)
+        {
+            var student = await _dbContext.Students.FindAsync(viewModel.Id);
+
+            if (student != null)
+            {
+                student.Name = viewModel.Name;
+                student.email = viewModel.email;
+                student.phone = viewModel.phone;
+                student.city = viewModel.city;
+                await _dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel) {
+            var student = await _dbContext.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x=>x.Id == viewModel.Id);
+            if(student != null)
+            {
+                _dbContext.Students.Remove(viewModel);
+                await _dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+
+        }
     }
 }
+
+
--- StudentPortal/obj/Debug/net8.0/StudentPortal.AssemblyInfo.cs
+++ StudentPortal/obj/Debug/net8.0/StudentPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ac90f888d2f998b98ba6053cce5d15773277f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
